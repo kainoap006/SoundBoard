489e2ebb43c2fc0a1d6fce8a9fefedecc2691827
kainoa-potter <48067302+kainoap006@users.noreply.github.com>
2024-05-04 16:49:46 -0700

    Keyboard functionality

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 function play_sound(clicked_id) {
-    var audio = new Audio(clicked_id + ".mp3");
+    var audio = new Audio("sounds/" + clicked_id + ".mp3");
     audio.play();
   }
+
+document.addEventListener('keypress', (event) => {
+  var keyCode = event.code;
+  if (keyCode === 'Digit1') {
+      play_sound("applause");
+  }
+  if (keyCode === 'Digit2') {
+      play_sound("RIZZ");
+  }
+  if (keyCode === 'Digit3') {
+      play_sound("china");
+  }
+  if (keyCode === 'Digit4') {
+    play_sound("Another Banger");
+  }
+  if (keyCode === 'Digit5') {
+    play_sound("Ruh Roh Raggy");
+  }
+});2
